6323346c36dc9c55ee1864dc9f4fb53a389031e8
Mary <marysalemme1@gmail.com>
2017-05-15 21:12:18 +0000

    Session1 completed!

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,14 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+    ary = string.split(//)
+    ary2 = []
+    ary.each_with_index do |element, index|
+        if (element.downcase == "r" && index < ary.length - 1)
+            ary2 << ary[index + 1]
+        end
+    end
+    ary2.join
+            
+    
 end
