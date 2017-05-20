d01eae2af1b2ae597c3b3dc6ce1ebcbf67772bf1
Mary <marysalemme1@gmail.com>
2017-05-20 14:42:59 +0000

    12_class.rb is done

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -1,8 +1,11 @@
 # Define a class called Fraction
 # This class is used to represent a ratio of two integers
-# Include setter methods called numerator and denominator that allow the user to change these values
-# Also include a method called to_f that returns the ratio as a float (a number with a decimal point, such as 10.3)
-# Also include a method called lowest, which returns a new Fraction, where the numerator and denominator
+# Include setter methods called numerator and denominator that allow the user to 
+# change these values
+# Also include a method called to_f that returns the ratio as a float 
+#(a number with a decimal point, such as 10.3)
+# Also include a method called lowest, which returns a new Fraction, 
+# where the numerator and denominator
 # are reduced to lowest terms (ie 20/60 becomes 1/3)
 # This will require finding the greatest common divisor for the numerator and denominator
 # then dividing both by that number.
@@ -21,8 +24,32 @@
 # f.to_f               # => 0.5
 
 class Fraction
-  def gcd(a,b)
+  attr_accessor :numerator, :denominator
+  def initialize(numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+  
+  def to_f
+    numerator.to_f/denominator
+  end
+  
+  def to_s
+    "#{numerator}/#{denominator}"
+  end
+  
+  def lowest
+    Fraction.new((numerator/gcd(numerator, denominator)),(denominator/gcd(numerator, denominator)))  
+  end
+  
+  def gcd(a, b)
     return a if b == 0
     gcd(b, a%b)
   end
 end
+f = Fraction.new 18, 56
+# p f.to_f
+# p f.numerator
+# p f.denominator+
+# p f.to_s
+# p f.lowest.to_s
