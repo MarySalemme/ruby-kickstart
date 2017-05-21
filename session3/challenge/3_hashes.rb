b38f784a15a6b6d5bbbe69a0512b321b446f229a
Mary <marysalemme1@gmail.com>
2017-05-21 11:36:39 +0000

    Challenge 3 completed

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,18 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+def word_count(str)
+    word_recurrence = {}
+    words = str.downcase.gsub(/[[:punct:]]/, "").split
+    words.each do |word| 
+        word_recurrence[word] = words.count(word) 
+    end
+    word_recurrence
+end
+
+
+
+
+p word_count "The dog!! and, the cat."
