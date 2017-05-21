0e97f1bfa10b8dec56396ac2339f0f1cd33b630c
Mary <marysalemme1@gmail.com>
2017-05-21 11:50:57 +0000

    Challenge 4 completed

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,14 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str)
+    word_position = {}
+    words = str.gsub(/[[:punct:]]/,"").split
+    words.each do |word|
+        word_position[word] = words.index(word)
+    end
+    word_position
+end
+
+p first_pos "The dog and the cat and the cow"
